dc9add0e6ccf50a543c918643b09909e9bf93772
chylli-deriv <52912308+chylli-deriv@users.noreply.github.com>
2023-07-14 10:11:54 +0800

    revert/perl_5.36.0 (#22)
    * Revert "upgrade/perl_5.36.0 (#21)"
    
    This reverts commit fcd7e7641f898c551d3685196ac44eddd47f113d.
    
    * revert/perl_5.36.0

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-VERSION=5.36.0
+wget http://www.cpan.org/src/5.0/perl-5.26.2.tar.gz
+rm -rf perl-5.26.2
+tar xzvf perl-5.26.2.tar.gz
+cd perl-5.26.2
 
-wget http://www.cpan.org/src/5.0/perl-$VERSION.tar.gz
-rm -rf perl-$VERSION
-tar xzvf perl-$VERSION.tar.gz
-cd perl-$VERSION
+# Apply patch for fixing Time::Local tests entering year 2020  (CPAN RT#124787)
+zcat ../patches/0001-Fix-Time-Local-tests.patch.gz | patch -p1 --verbose || exit 1
 
-./Configure -DSILENT_NO_TAINT_SUPPORT -Dusesitecustomize -Dinc_version_list=none -Dprefix=/home/git/binary-com/perl -Dsitelib=/home/git/regentmarkets/cpan-private/local/lib/perl5 -Dsitearch=/home/git/regentmarkets/cpan-private/local/lib/perl5/x86_64-linux  -Dvendorprefix=/home/git/regentmarkets/cpan/local -Dvendorlib=/home/git/regentmarkets/cpan/local/lib/perl5 -Dvendorarch=/home/git/regentmarkets/cpan/local/lib/perl5/x86_64-linux -Duselargefiles -Dccflags="-DDEBIAN -D_FORTIFY_SOURCE=2 -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security" -Dldflags=" -Wl,-z,relro" -Dlddlflags="-shared -Wl,-z,relro" -Dcccdlflags="-fPIC" -Duse64bitint -Dman1dir=none -Dman3dir=none -Dpager=/usr/bin/sensible-pager -Uafs -Ud_csh -Ud_ualarm -Uusesfio -Uusenm -Uuseithreads -Uusemultiplicity -Ui_libutil -DDEBUGGING=-g -Doptimize=-O2 -Duseshrplib -des
+# Apply patch for fixing gcc 10 problem  https://github.com/openwrt/packages/pull/12178
+patch -Np1 < ../patches/999-fix-build-failure-against-gcc-10.patch
+
+./Configure -Dusesitecustomize -Dinc_version_list=none -Dprefix=/home/git/binary-com/perl -Dsitelib=/home/git/regentmarkets/cpan-private/local/lib/perl5 -Dsitearch=/home/git/regentmarkets/cpan-private/local/lib/perl5/x86_64-linux  -Dvendorprefix=/home/git/regentmarkets/cpan/local -Dvendorlib=/home/git/regentmarkets/cpan/local/lib/perl5 -Dvendorarch=/home/git/regentmarkets/cpan/local/lib/perl5/x86_64-linux -Duselargefiles -Dccflags="-DDEBIAN -D_FORTIFY_SOURCE=2 -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security" -Dldflags=" -Wl,-z,relro" -Dlddlflags="-shared -Wl,-z,relro" -Dcccdlflags="-fPIC" -Duse64bitint -Dman1dir=none -Dman3dir=none -Dpager=/usr/bin/sensible-pager -Uafs -Ud_csh -Ud_ualarm -Uusesfio -Uusenm -Uuseithreads -Uusemultiplicity -Ui_libutil -DDEBUGGING=-g -Doptimize=-O2 -Duseshrplib -des
 make && make test && make install
 
